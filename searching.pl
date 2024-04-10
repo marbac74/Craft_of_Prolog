3f366918aa79c4e88ea3604fb18479bc6d5ce8d9
Marco Bachini <marbac74@hotmail.com>
2024-04-10 16:58:13 +0200

    Depth-first and breadth-first search

--- searching.pl
+++ searching.pl
@@ -1,4 +1,5 @@
-% Depth-first search
+% A graph
+
 start(a).
 solution(l).
 
@@ -17,6 +18,7 @@ child(f, m).
 child(g, n).
 child(g, o).
 
+% Searching using "natural" Prolog depth-first search strategy
 
 answer(Y) :-
     start(X),
@@ -28,7 +30,7 @@ child_star(X, Z) :-
     child(X, Y),
     child_star(Y, Z).
 
-% Explicit depth-first search
+% Searching with explicit depth-first search
 
 children(ParentNode, ChildrenNodes) :-
     findall(ChildNode, child(ParentNode, ChildNode), ChildrenNodes).
@@ -44,7 +46,7 @@ depth_star([X|Open1], Y) :-
     append(Children, Open1, Open2),
     depth_star(Open2, Y).
 
-% Breadth-first search
+% Searching with breadth-first search
 
 breadth_first_1(Answer) :-
     start(Start),
@@ -56,3 +58,31 @@ breadth_star_1([X|Open1], Y) :-
     children(X, Children),
     append(Open1, Children, Open2),
     breadth_star_1(Open2, Y).
+
+% Implementing a queue in Prolog
+% Adding items one at a time
+
+empty_queue_1([]).
+queue_head_1(Head, Queue, [Head|Queue]).
+queue_last_1(Last, Queue, NewQueue) :-
+    append(Queue, [Last], NewQueue).
+
+% Adding items in groups
+
+queue_head_list_1(Heads, Queue, NewQueue) :-
+    append(Heads, Queue, NewQueue).
+queue_last_list_1(Lasts, Queue, NewQueue) :-
+    append(Queue, Lasts, NewQueue).
+
+% With list pairs
+
+empty_queue_2([]+[]).
+
+queue_head_2(Head, L1+R1, L2+R2) :-
+    queue_head_2(L2, R2, L1, R1, Head).
+queue_head_2([], R, L, [], Head) :-
+    reverse(R, [Head|L]).
+queue_head_2([Head|L], R, L, R, Head).
+
+queue_last_2(Last, L1+R1, L2+R2) :-
+    queue_head_2(R2, L2, R1, L1, Last).
